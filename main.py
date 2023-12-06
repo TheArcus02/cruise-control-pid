60a14a2fb5444b7c0d6a6257d3b62d2b6230f632
TheArcus02 <m.urbaniak.inf@gmail.com>
2023-12-06 10:39:10 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from dash import Dash, html
+import pandas as pd
+import numpy as np
+from scipy.integrate import odeint
+
+def main(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    main('PyCharm')
